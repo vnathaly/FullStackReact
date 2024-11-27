ed7fcbff3b7e6ea418b5aa4aa2fc04a8db3875c7
Nathaly Victoriano <nathalyvictorianodiaz2020fb@gmail.com>
2024-11-27 12:11:13 -0300

    I deleted all that i dont need it

--- src/App.jsx
+++ src/App.jsx
@@ -16,20 +16,4 @@ const App = () => {
   )
 }
 
-function Avatar () {
-  return (
-    <img 
-    className="Avatar"
-    src="https://i.imgur.com/1bX5QH6.jpg" 
-    alt="Happy Old"
-    width={100}
-    height={100}
-    />
-   );
-}
-
-export default function Profile() {
-  return (
-    <Avatar />
-  );
-}
+export default App
